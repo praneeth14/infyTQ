b7669f9473733f9fe41a5354e05dbc8c806f4874
praneeth14 <ballarapu.praneeth@gmail.com>
2019-06-15 18:34:09 +0530

    A25

---
+++ Programming Fundaments using Python/A25.py
@@ -0,0 +1,13 @@
+#PF-Tryout
+
+#debug the below code
+counter1=0
+counter2=5
+while(counter1 < 5):
+  star=""
+  c2=counter2
+  while(c2>counter1):
+     star=star+ "*"
+     c2-=1
+  print(star)
+  counter1+=1
