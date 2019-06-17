2a5a9d9924bb43908b8cc67e392f738a3e7aa0fc
praneeth14 <ballarapu.praneeth@gmail.com>
2019-06-17 10:02:47 +0530

    A39 Updated

--- Programming Fundaments using Python/Assignments/A39.py
+++ Programming Fundaments using Python/Assignments/A39.py
@@ -3,32 +3,37 @@
 
 # Global variables
 menu = ('Veg Roll', 'Noodles', 'Fried Rice', 'Soup')
-quantity_available = [2, 200, 250, 3]
+quantity_available = [2, 200, 3, 0]
 
 '''This method accepts the item followed by the quantity required by a customer in the format item1, quantity_required, item2, quantity_required etc.'''
 
 
 def place_order(*item_tuple):
-    for i in range(0, len(item_tuple),2):
+    for i in range(0, len(item_tuple), 2):
         if item_tuple[i] not in menu:
-            print(item_tuple[i],"is not available")
+            print(item_tuple[i], "is not available")
+        elif (check_quantity_available(menu.index(item_tuple[i]), item_tuple[i + 1])):
+            print(item_tuple[i], "is available")
+        else:
+            print(item_tuple[i], "stock is over")
 
     # Populate the item name in the below given print statements
     # Use it to display the output wherever applicable
     # Also, do not modify the text in it for verification to work
-
-    # print("<item name>stock is over")
-    # print ("<item name>is available")
 
 
 '''This method accepts the index position of the item requested by the customer in the quantity_available list, and the requested quantity of the item.'''
 
 
 def check_quantity_available(index, quantity_requested):
-    pass
-    # Remove pass and write your logic here to return an appropriate boolean value.
+    if quantity_available[index] >= quantity_requested:
+        quantity_available[index] -= quantity_requested
+        return True
+    else:
+        return False
 
 
 # Provide different values for items ordered and test your program
-place_order("Veg Roll", 2, "Noodles", 2)
-place_order("Soup", 1, "Veg Roll", 2, "Fried Rice1", 1)
+# place_order("Veg Roll", 2, "Noodles", 2)
+# place_order("Soup", 1, "Veg Roll", 2, "Fried Rice1", 1)
+place_order("Fried Rice", 2, "Soup", 1)
